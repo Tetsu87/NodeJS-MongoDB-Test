9fd6b4f279bfb4c10ffffdf9057d66efaf272fa0
Tkai87 <tkai@usc.edu>
2022-08-27 14:55:03 -0700

    added mongoDB setting

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+var config = require("./config");
+
+mongoose.connect(
+    `mongodb+srv://Polymath:${config.mongoPW}@polymath.7dzdr87.mongodb.net/?retryWrites=true&w=majority`
+    );
 
 app.listen(3000, ()=> {
     console.log("server is runninng!!!");
